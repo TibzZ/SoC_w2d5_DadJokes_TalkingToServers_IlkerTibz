15c3939986bf0098ae8cac58fc2f292b4e21319a
Tibz <thibaut_marechal@yahoo.com>
2021-01-15 10:49:07 +0000

    Plan plus first functions fetching joke

--- main.js
+++ main.js
@@ -1,7 +1,47 @@
+// I) EXTRACTING API DATA
+// We retrieve information for Dad Joke API 
+
+let resultJoke = document.querySelector("h2")
+
+
 async function fetchJoke() {
   let response = await fetch("https://icanhazdadjoke.com/", {
     headers: { accept: "application/json" },
   });
   let result = await response.json();
+  // Get the ID of the joke
+  console.log(result.id);
+  // We get the related joke
   console.log(result.joke);
+  resultJoke.innerText = result.joke
 }
+
+fetchJoke();
+
+// We want to retrieve a picture 
+
+
+// We want to be able to search for a joke
+
+// async function imageJoke() {
+//   let response = await fetch("https://icanhazdadjoke.com/", {
+//     headers: { accept: "application/json" },
+//   });
+//   let result = await response.json();
+//   console.log(result.id);
+//   return result.joke;
+// }
+
+
+// Test Print result in h2
+
+// 2 PAGE LAYOUT CUSTOMISATION
+    // Design background plus title 
+
+    // Create Buttons for joke to be categorised as funny or 📓 
+
+    //Need store values in one of the two following sections:
+          //1- Funny 
+          //2- Not Funny
+
+  // option to send funny/not funny quotes to people social media/📧 
